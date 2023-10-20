c965ca5bff830924520fb156171e6bedefd39e5d
Licheng Mao <tavimori@gmail.com>
2023-10-20 18:20:21 +0000

    backport tests

--- tests/tile_image_render.rs
+++ tests/tile_image_render.rs
@@ -4,9 +4,10 @@ use std::io::Read;
 
 #[test]
 fn main() {
+    let mut fogmap = FogMap::new();
     let mut tile_file = File::open("tests/0921iihwtxn").unwrap();
     let mut content = Vec::new();
     tile_file.read_to_end(&mut content).unwrap();
     println!("loading a file with len{}.", content.len());
-    let tile = Tile::create("0921iihwtxn", content);
+    fogmap.add_fow_file("0921iihwtxn", content);
 }
