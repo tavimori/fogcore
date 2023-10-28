79a850673afa8e97222fd53c805f1cb91fa772f8
tavimori <tavimori@users.noreply.github.com>
2023-10-28 13:14:05 +0000

    deploy: d0888508e25652164742db8e7d879e651b6607d7

---
+++ fogcore/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["greet"],"mod":["fogmaps","renderer"]};
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"fogcore":["",[],["fogmaps.rs","lib.rs","renderer.rs","utils.rs"]]\
+}');
+createSrcSidebar();
