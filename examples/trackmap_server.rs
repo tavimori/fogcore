3066618902df0f329bfadc18e001d2b2fa5a2b85
Licheng Mao <tavimori@gmail.com>
2024-11-19 19:59:57 +0800

    add mini server

--- examples/trackmap_server.rs
+++ examples/trackmap_server.rs
@@ -5,6 +5,7 @@ use actix_web_actors::ws;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use serde_json;
+use std::borrow::Cow;
 
 // WebSocket message types
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,8 +28,9 @@ impl Actor for MapSocket {
         let handle = ctx.run_interval(std::time::Duration::from_secs(5), |act, ctx| {
             let command = MapCommand {
                 command: "fly_to".to_string(),
-                lat: rand::thread_rng().gen_range(-90.0..90.0),
-                lng: rand::thread_rng().gen_range(-180.0..180.0),
+                // range of lat/lng for roughly mainland PRC
+                lat: rand::thread_rng().gen_range(30.0..53.0),
+                lng: rand::thread_rng().gen_range(73.0..135.0),
                 zoom: Some(rand::thread_rng().gen_range(3.0..12.0)),
             };
 
@@ -73,16 +75,51 @@ async fn index() -> HttpResponse {
         .body(include_str!("../static/tracks-client.html"))
 }
 
+const FOGCORE_JS: &str = include_str!("../pkg/fogcore.js");
+const FOGCORE_WASM: &[u8] = include_bytes!("../pkg/fogcore_bg.wasm");
+
+const TRACKS_LAYER_JS: &str = include_str!("../static/tracks-layer.js");
+
+const TILES_DATA: &[u8] = include_bytes!("../static/tiles.zip");
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server at http://127.0.0.1:5503");
+    println!("Starting server at http://localhost:5503");
 
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/ws", web::get().to(ws_route))
+            .route("/fogcore.js", web::get().to(serve_fogcore_js))
+            .route("/fogcore_bg.wasm", web::get().to(serve_fogcore_wasm))
+            .route("/tracks-layer.js", web::get().to(serve_tracks_layer_js))
+            .route("/tiles.zip", web::get().to(serve_tiles_zip))
     })
-    .bind("127.0.0.1:5503")?
+    .bind("localhost:5503")?
     .run()
     .await
+}
+
+async fn serve_fogcore_js() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/javascript")
+        .body(FOGCORE_JS)
+}
+
+async fn serve_fogcore_wasm() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/wasm")
+        .body(Cow::Borrowed(FOGCORE_WASM))
+}
+
+async fn serve_tracks_layer_js() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/javascript")
+        .body(TRACKS_LAYER_JS)
+}
+
+async fn serve_tiles_zip() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/zip")
+        .body(Cow::Borrowed(TILES_DATA))
 }
